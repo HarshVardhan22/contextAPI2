c7f021c8ecb5ed02fff78c9daa88baff0cfda857
HarshVardhan22 <hsinha31@gmail.com>
2020-11-16 23:22:28 +0530

    context feature added

--- src/MovieContext.js
+++ src/MovieContext.js
@@ -1,31 +1,31 @@
-// import React,{createContext,useState} from 'react';
+import React,{createContext,useState} from 'react';
 
-// export const MovieContext = createContext();
+export const MovieContext = createContext();
 
-// export const MovieProvider = (props) =>{
-//     const [movies,setMovies] = useState([
-//         {
-//           name: "Prestige",
-//           price:300,
-//           id:1
-//         },
-//         {
-//           name: "Sholay",
-//           price:350,
-//           id:2
-//         },
-//         {
-//           name:"Gangs of Wasseypur",
-//           price:3000,
-//           id:3
-//         }
+export const MovieProvider = (props) =>{
+    const [movies,setMovies] = useState([
+        {
+          name: "Prestige",
+          price:300,
+          id:1
+        },
+        {
+          name: "Sholay",
+          price:350,
+          id:2
+        },
+        {
+          name:"Gangs of Wasseypur",
+          price:3000,
+          id:3
+        }
       
-//     ])
-//     return(
-//         <MovieContext.Provider value={[movies,setMovies]}>
-//             {props.children}
-//         </MovieContext.Provider>
-//     );
-// }
+    ])
+    return(
+        <MovieContext.Provider value={[movies,setMovies]}>
+            {props.children}
+        </MovieContext.Provider>
+    );
+}
 
  
